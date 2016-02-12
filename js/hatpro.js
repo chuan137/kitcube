3425a8f6fae5a45393f42f0abe99a43c9ba99adc
Chuan Miao <chuan137@gmail.com>
2016-02-12 16:33:16 +0100

    refactor javascript

--- js/hatpro.js
+++ js/hatpro.js
@@ -1,4 +1,36 @@
- $(document).ready(function() {
+var plotData = require('./plotdata');
+var Base64 = require('./base64');
+
+function sensor(id, name, unit, unit2) {
+    var e = document.createElement('li');
+    var link = document.createElement('a');
+    e.setAttribute('id', id);
+    e.setAttribute('unit', unit);
+    e.setAttribute('title', name);
+    if (unit2 !== undefined)
+        e.setAttribute('unit2', unit2);
+    link.setAttribute('href', '#');
+    link.innerHTML = name;
+    e.appendChild(link);
+    return e;
+}
+
+function updateData(script, id ,date) {
+    $.ajax({
+        url: 'browserify/python/hatpro/'+script + '?id='+id + '&date='+date,
+        method: 'post',
+    });
+}
+
+function switchHighlight() {
+        $(this).siblings().each(function() {
+                $(this).removeClass("highlight");
+        });
+        $(this).addClass("highlight");
+}
+        
+module.exports = function() {
+
     $('#HATPRO #timeseries ul').append(sensor('L2A.ATM.WAT.VAP.CNT', 'water-vapor content', 'kg m-2').outerHTML);
     $('#HATPRO #timeseries ul').append(sensor('L2A.ATM.LIQ.WAT.CNT', 'liquid-water content', 'g m-2').outerHTML);
     $('#HATPRO #timeseries ul').append(sensor('L1B.BRIGHT.TEMP', 'brightness temperature', 'K').outerHTML);
@@ -11,45 +43,14 @@
     $('#HATPRO #profile ul').append(sensor('L2A.ATM.LIQ.WAT.CNT.PRF', 'liquid-water content', 'azimuth angle', 'g m-2').outerHTML);
     $('#HATPRO #profile ul li').first().addClass('highlight');
 
-    setInterval(function update() {
-        updateData('hatpro_time.cgi', 'L2A.ATM.WAT.VAP.CNT', '0d');
-        updateData('hatpro_time.cgi', 'L2A.ATM.WAT.VAP.CNT', '1d');
-        updateData('hatpro_time.cgi', 'L2A.ATM.WAT.VAP.CNT', '2d');
-        updateData('hatpro_time.cgi', 'L2A.ATM.LIQ.WAT.CNT', '0d');
-        updateData('hatpro_time.cgi', 'L2A.ATM.LIQ.WAT.CNT', '1d');
-        updateData('hatpro_time.cgi', 'L2A.ATM.LIQ.WAT.CNT', '2d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP', '0d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP', '1d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP', '2d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP.IR', '0d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP.IR', '1d');
-        updateData('hatpro_time.cgi', 'L1B.BRIGHT.TEMP.IR', '2d');
-        updateData('hatpro_contour.cgi', 'L2C.AIR.POT.TEM.PRF', '0d');
-        updateData('hatpro_contour.cgi', 'L2C.AIR.POT.TEM.PRF', '1d');
-        updateData('hatpro_contour.cgi', 'L2C.AIR.POT.TEM.PRF', '2d');
-        updateData('hatpro_contour.cgi', 'L2C.REL.HUM.PRF', '0d');
-        updateData('hatpro_contour.cgi', 'L2C.REL.HUM.PRF', '1d');
-        updateData('hatpro_contour.cgi', 'L2C.REL.HUM.PRF', '2d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.WAT.VAP.CNT', '0d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.WAT.VAP.CNT', '1d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.WAT.VAP.CNT', '2d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.LIQ.WAT.CNT', '0d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.LIQ.WAT.CNT', '1d');
-        updateData('hatpro_content.cgi', 'L2A.ATM.LIQ.WAT.CNT', '2d');
- 
-
-        }, 300*1000);
-
-    $( "#radiotime" ).buttonset();
-
     $(".navmenu#timeseries li").each(function(index) {
         $(this).on("click", switchHighlight);
-        $(this).on("click", {target: '#HATPRO_1D'}, makePlotD1);
+        $(this).on("click", {target: '#HATPRO_1D'}, plotData.plot_time);
       });
 
     $(".navmenu#profile li").each(function(index) {
         $(this).on("click", switchHighlight);
-        $(this).on("click", makePlotD2);
+        $(this).on("click", plotData.plot_contour);
       });
 
     $(".navmenu li a").each(function() {
@@ -64,26 +65,5 @@
     $(function() {
       $(".highlight").click();
     }); 
-
-    $('.printbutton').button();
-    $('.printbutton').click(function()  {
-      var img1 = $(this).parents("div .ui-widget-content").data('img1');
-      var img2 = $(this).parents("div .ui-widget-content").data('img2');
-      var title1 = $(this).parents("div .ui-widget-content").data('title1');
-      var title2 = $(this).parents("div .ui-widget-content").data('title2');
-
-      w = window.open();
-      w.document.write("<div style='text-align: center'><b>");
-      w.document.write(title1);
-      w.document.write("</b></div>");
-      w.document.write("<img src='"+img1+"'/>");
-      w.document.write("<br><br><div style='text-align: center'><b>");
-      w.document.write(title2);
-      w.document.write("</b></div>");
-      w.document.write("<img src='"+img2+"'/>");
-      w.print();
-      w.close();
-    });
-
-  });
+};
 
---
+++ js/plotdata.js
@@ -0,0 +1,308 @@
+function formatDate(date) {
+    var dd = date.getDate()
+    if ( dd < 10 ) dd = '0' + dd;
+    var mm = date.getMonth()+1
+    if ( mm < 10 ) mm = '0' + mm;
+    var yy = date.getFullYear();
+    if ( yy < 10 ) yy = '0' + yy;
+    return dd+'.'+mm+'.'+yy;
+}
+
+function plot_time(event) {
+    var panelname = $(this).parents('div .ui-widget-content').attr('id');
+    var timekey = $('#' + panelname + ' input[type=radio]:checked').val();
+    var id = $(this).attr('id');
+    var title = $(this).attr('title');
+    console.log(title);
+    var targetdiv = event.data.target;
+    var targetdivwidth = parseFloat($(targetdiv).css('width'));
+    var unit = $(this).attr('unit');
+
+
+    $.ajax({
+        url: 'cache/' + panelname + '.' + id + '.' + timekey + '.json',
+        method: 'post',
+        dataType: 'json',
+        data: {timekey: timekey, id: id},
+        success: plotdata
+    });
+
+    function plotdata(data) {
+        var dataset = [];
+        var rows = data['data'].length;
+
+        var factor = Math.floor(data['time'].length/targetdivwidth/3 + 0.5);
+        if (factor == 0) factor = 1;
+        console.log(factor);
+
+        for (var w = 0; w < rows; w++) {
+          var temp = [];
+          for (var i = 0; i < data['time'].length; i+=factor) {
+              temp.push([data['time'][i]*1000, data['data'][w][i]]);
+          }
+          dataset.push(temp);
+        }
+
+        var d0 = new Date(data['time'][0]*1000);
+        var dmin = Date.UTC(d0.getUTCFullYear(), d0.getUTCMonth(), d0.getUTCDate(), 0, 0, 0);
+        var dmax = dmin + 86400000;
+        var hour = 3600000; 
+        var ticks = [];
+        for (var i=0; i<=24; i+=3) ticks.push(dmin + i*hour);
+
+        var options = {
+            canvas: true,
+            xaxis: { 
+                mode: "time",
+                ticks: ticks,
+                min: dmin,
+                max: dmax,
+                timeformat: "%H:%M",
+                axisLabelUseCanvas: true,
+                axisLabelPadding: 16,
+                axisLabelFontSizePixels: 16,
+                axisLabelFontFamily: 'Arial',
+                axisLabelColour: 'black',
+                axisLabel: 'UTC Time   on   ' + formatDate(new Date(dmin)),
+            },
+            yaxis: {
+                labelWidth: 40,
+                axisLabelUseCanvas: true,
+                axisLabelFontFamily: 'Arial',
+                axisLabel: '[ ' + unit + ' ]'
+            },
+            series: {
+                shadowSize: 0,
+            },
+        }
+
+        var newplot = $.plot(targetdiv, dataset, options);
+
+        /*
+        var divholder = document.createElement('div');
+        var divelement = document.createElement('div');
+        divelement.style.width=600+'px';
+        divelement.style.height=300+'px';
+        var title = document.createElement('p');
+        title.innerHTML = 'abc';
+        divholder.appendChild(title);
+        divholder.appendChild(divelement);
+        var printversion = $.plot(divelement, dataset, options);
+        console.log(printversion);
+        */
+
+        var canvas = newplot.getCanvas();
+        var img = canvas.toDataURL("img/png");
+
+        $('div #'+panelname).data('img1', img);
+        $('div #'+panelname).data('title1', title);
+    }
+}
+
+function plot_contour() {
+    var panelname = $(this).parents("div .ui-widget-content").attr('id');
+    var time_choice = $('#' + panelname + ' input[type=radio]:checked').val();
+    var unit = $(this).attr('unit');
+    var unit2 = $(this).attr('unit2');
+    var title = $(this).attr('title');
+
+    $.ajax({
+        url: "cache/" + panelname + "." +  $(this).attr('id') + "." + time_choice + ".json",
+        method: "post",
+        dataType: "json",
+        data: {time_choice: time_choice, id: $(this).attr('id')},
+        success: plotdata2
+    });
+
+    function plotdata2(data) {
+        var d0 = new Date(data["xmin"]*1000);
+        var dmin = Date.UTC(d0.getUTCFullYear(), d0.getUTCMonth(), d0.getUTCDate(), 0, 0, 0);
+        var dmax = dmin + 86400000;
+        var hour = 3600000; 
+        var ticks = [];
+        for (var i=0; i<=24; i+=3) ticks.push(dmin + i*hour);
+
+        var options_left = {
+            canvas: true,
+          series: { 
+              lines: {
+                  lineWidth: 1
+              }, 
+              shadowSize: 0.5, 
+          },
+          xaxis: { 
+              mode: 'time',
+              min: dmin, 
+              max: dmax, 
+              ticks: ticks,
+              timeformat: "%H:%M",
+              axisLabelUseCanvas: true,
+              axisLabelPadding: 16,
+              axisLabelFontSizePixels: 16,
+              axisLabelFontFamily: 'Arial',
+              axisLabelColour: 'black',
+              axisLabel: 'UTC Time   on   ' + formatDate(new Date(dmin)),
+          },
+          yaxis: { 
+              min: data["ymin"], 
+              max: data["ymax"],
+              labelWidth: 40,
+              axisLabelUseCanvas: true,
+              axisLabelFontFamily: 'Arial',
+              axisLabel: '[ ' + unit + ' ]'
+          },
+          grid: { 
+              margin: {} 
+          },
+          hooks: { 
+              drawSeries: [fillShape] 
+          }
+        }
+
+    var dataset=[];
+    
+    for (var i = 0; i < data['data'].length; i++) {
+        for (var j = 0; j < data['data'][i]["path"].length; j++) {
+            if (data['data'][i]["path"][j] != null)
+                data['data'][i]["path"][j][0] *= 1000;
+        }
+        dataset.push({
+            data: data['data'][i]["path"], 
+            color: data['data'][i]["color"]});
+    }
+
+    function fillShape(plot, ctx, series) {
+
+		var plotOffset = plot.getPlotOffset();
+		var offset_x = plotOffset.left;
+		var offset_y = plotOffset.top;
+
+		function coord_x(x) {
+			return offset_x + series.xaxis.p2c(x)
+		};
+		function coord_y(y) {
+			return offset_y + series.yaxis.p2c(y)
+		};
+
+		var data = series.data;
+		var chk = 0;
+
+		ctx.beginPath();
+		for (var i = 0; i < data.length; i++) {
+			if (data[i] == null) {
+				chk = 0;
+				continue;
+			}
+			if (chk == 0) {
+				ctx.moveTo(coord_x(data[i][0]), coord_y(data[i][1]));
+				chk = 1;
+			} else {
+				ctx.lineTo(coord_x(data[i][0]), coord_y(data[i][1]));
+			}
+		}
+		ctx.fillStyle = series.color;
+		ctx.fill();
+		ctx.closePath();
+    }
+
+    var colorbar = [];
+    var colorbar_val = [];
+    var color = '';
+    var value = -1000;
+    for (var i=0; i<data['data'].length; i++) {
+        var newcolor = data['data'][i]['color'];
+        var newval = data['data'][i]['layer'];
+        if (newval < value) {
+            break;
+        } else {
+            value = newval;
+        }
+        if (newcolor !== color) {
+            colorbar.push(newcolor);
+            colorbar_val.push(newval);
+            color = newcolor;
+        }
+    }
+
+    var ymin = colorbar_val[0];
+    var ymax = colorbar_val[colorbar_val.length-1];
+
+    console.log(colorbar, colorbar_val);
+
+    var cntrArr = []; //last points
+    var tempObj = {
+        data: [],
+        color: "",
+    };
+    
+    var cntDataset = [];
+    for (var i=0; i<data['data'].length; i++) {
+        cntDataset.push({
+            data: [[0,colorbar_val[i]],
+                   [1,colorbar_val[i]],
+                   [2,colorbar_val[i]]],
+            color: colorbar[i]
+        });
+    }
+
+        var options_right = {
+            canvas: true,
+          series: { 
+              lines:{
+                  fillColor: {colors: colorbar},
+                  fill: true,
+              }, 
+              shadowSize: 0, 
+          },
+          xaxis: {
+              axisLabel:  unit2,
+              axisLabelPadding: 16,
+              tickLength: 0,
+              ticks: [0, 1],
+              tickColor: 'transparent',
+              tickFormatter: function(val, axis) {
+                  return ""
+              },
+              axisLabelUseCanvas: true,
+              axisLabelFontSizePixels: 12,
+              axisLabelFontFamily: 'Arial',
+          },
+          yaxis: { 
+              min: ymin, 
+              max: ymax,
+              position: "right",
+              axisLabelUseCanvas: true,
+              ticks: 10
+          },
+          grid: { 
+              margin: {left:0, right:0, bottom: 17} 
+          },
+          hooks: { 
+          }
+        }
+
+        console.log('colorbar', cntDataset);
+        console.log(cntDataset.length);
+
+        var plot1 = $.plot("#plot2d", dataset, options_left);
+        var plot2 = $.plot("#plotColorBar", cntDataset, options_right);
+
+        var canvas1 = plot1.getCanvas();
+        var canvas2 = plot2.getCanvas();
+
+        var canvas3 = document.createElement('canvas');
+        canvas3.width = 800;
+        canvas3.height = 400;
+        var ctx3 = canvas3.getContext('2d');
+
+        ctx3.drawImage(canvas1, 0, 10);
+        ctx3.drawImage(canvas2, 700, 10);
+
+        var img = canvas3.toDataURL("img/png");
+        $('div #'+panelname).data('img2', img);
+        $('div #'+panelname).data('title2', title);
+  }
+}
+
+exports.plot_time = plot_time;
+exports.plot_contour = plot_contour;
