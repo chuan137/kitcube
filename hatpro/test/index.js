dddbbb5bf893ba75fc8a9fabe0e6726fe6e8072b
Chuan Miao <chuan137@gmail.com>
2016-07-04 20:23:14 +0200

    Add test for HEADS

--- hatpro/test/index.js
+++ hatpro/test/index.js
@@ -7,7 +7,8 @@ require(["../js/hatpro.js",
          "script!flot/jquery.flot.canvas"], function(hatpro) {
 
   $("#HATPRO").load('content.html', function() {
-    hatpro('DACCIWA', '2016-06-29 UTC');
+    // hatpro('DACCIWA', '2016-06-29 UTC');
+    hatpro('HEADS', '2014-08-15 UTC');
     // hatpro('DACCIWA');
   });
 });
